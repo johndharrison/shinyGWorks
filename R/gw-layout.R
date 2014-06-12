2314e474801a3fa25c9babb21d9db5f37581a339
johndharrison <johndharrison0@gmail.com>
2014-06-12 23:33:42 +0700

    Add gwfluidrow and gwcolumn

--- R/gw-layout.R
+++ R/gw-layout.R
@@ -10,9 +10,11 @@ gwfluidPage <- function(...){
 }
 
 #' @rdname gwfluidPage
+#' @param class additional class definitions to add.
 #' @export
-gwfluidRow <- function(...){
-  div(class = "row", ...)  
+gwfluidRow <- function(..., class = NULL){
+  class <- ifelse(is.null(class), "row", paste("row", class))
+  div(class = class, ...)  
 }
 
 #' Create a gwcolumn within a UI definition
@@ -23,17 +25,22 @@ gwfluidRow <- function(...){
 #' @param denominator
 #' @param ... Elements to include within the column
 #' @param class additional class definitions to add.
+#' @param gridBreakpoint By default, grid columns collapse into rows at the $small-tablet breakpoint. The grid breakpoint helpers allow you to target the breakpoint that the grid structure should persist through.
+#' For more detail see \link{http://groundworkcss.github.io/groundwork/docs/breakpoints.htm}
 #'
 #' @return A gwcolumn that can be included within a
 #' \code{\link{gwfluidRow}}.
 #'
 #'
-#' @seealso \code{\link{gwfluidRow}}, \code{\link{gwfixedRow}}.
+#' @seealso \code{\link{gwfluidRow}}.
 #'
 #' @export
 
-gwcolumn <- function(nominator = '', denominator = c(1:12), ..., class = NULL){
+gwcolumn <- function(nominator = '', denominator = c(1:12), ..., class = NULL
+                     , gridBreakpoint = c(NA, "small-tablet", "mobile")){
+  gridBreakpoint <- match.arg(gridBreakpoint)
   class <- ifelse(is.null(class), fractionalClass(as.integer(nominator), as.integer(denominator))
                   , paste(fractionalClass(as.integer(nominator), as.integer(denominator)), class))
+  class <- ifelse(is.na(gridBreakpoint), class, paste(class, gridBreakpoint))
   div(class = class, ...)
 }
