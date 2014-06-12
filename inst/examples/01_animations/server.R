1324cdd6de2ab46d17d00a9eb09119024803a083
johndharrison <johndharrison0@gmail.com>
2014-06-12 23:34:49 +0700

    Add example for animations

--- inst/examples/01_animations/server.R
+++ inst/examples/01_animations/server.R
@@ -1,22 +1,5 @@
 library(shiny)
 library(shinyGWorks)
-animations <- c("flash", "bounce", "shake", "tada", "swing", "wobble", "pulse", 
-                "flip", "flipInX", "flipOutX", "flipInY", "flipOutY", 
-                "fadeIn", "fadeInUp", "fadeInDown", "fadeInLeft", 
-                "fadeInRight", "fadeInUpBig", "fadeInDownBig", "fadeInLeftBig", 
-                "fadeInRightBig", "fadeOut", "fadeOutUp", "fadeOutDown", 
-                "fadeOutLeft", "fadeOutRight", "fadeOutUpBig", "fadeOutDownBig", 
-                "fadeOutLeftBig", "fadeOutRightBig", "bounceIn", 
-                "bounceInDown", "bounceInUp", "bounceInLeft", "bounceInRight", 
-                "bounceOut", "bounceOutDown", "bounceOutUp", "bounceOutLeft", 
-                "bounceOutRight", "rotateIn", "rotateInDownLeft", 
-                "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", 
-                "rotateOut", "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", 
-                "rotateOutUpRight", "lightSpeedIn", "lightSpeedOut", 
-                "hinge", "rollIn", "rollOut", "spin", "spin-once", 
-                "spin-twice", "spin-thrice", "spin-fast", "spin-fast-once", 
-                "spin-fast-twice", "spin-fast-thrice", "spin-slow", "spin-slow-once", 
-                "spin-slow-twice", "spin-slow")
 
 shinyServer(function(input, output) {
   output$plot <- renderPlot({ hist(runif(input$n)) })
@@ -24,7 +7,7 @@ shinyServer(function(input, output) {
   output$test <- renderUI({
     gwcolumn(2, 3
              , plotOutput('plot')
-             , style = "background-color:green;", class = paste("left-one padded", input$anim,"animated"))
+             , style = "background-color:green;", class = paste("left-one padded box", input$anim,"animated"))
     
   })
 }
--- inst/examples/01_animations/ui.R
+++ inst/examples/01_animations/ui.R
@@ -1,4 +1,21 @@
 library(shiny)
+animations <- c("flash", "bounce", "shake", "tada", "swing", "wobble", "pulse", 
+                "flip", "flipInX", "flipOutX", "flipInY", "flipOutY", 
+                "fadeIn", "fadeInUp", "fadeInDown", "fadeInLeft", 
+                "fadeInRight", "fadeInUpBig", "fadeInDownBig", "fadeInLeftBig", 
+                "fadeInRightBig", "fadeOut", "fadeOutUp", "fadeOutDown", 
+                "fadeOutLeft", "fadeOutRight", "fadeOutUpBig", "fadeOutDownBig", 
+                "fadeOutLeftBig", "fadeOutRightBig", "bounceIn", 
+                "bounceInDown", "bounceInUp", "bounceInLeft", "bounceInRight", 
+                "bounceOut", "bounceOutDown", "bounceOutUp", "bounceOutLeft", 
+                "bounceOutRight", "rotateIn", "rotateInDownLeft", 
+                "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", 
+                "rotateOut", "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", 
+                "rotateOutUpRight", "lightSpeedIn", "lightSpeedOut", 
+                "hinge", "rollIn", "rollOut", "spin", "spin-once", 
+                "spin-twice", "spin-thrice", "spin-fast", "spin-fast-once", 
+                "spin-fast-twice", "spin-fast-thrice", "spin-slow", "spin-slow-once", 
+                "spin-slow-twice", "spin-slow")
 
 shinyUI(gwfluidPage(
   gwfluidRow(
